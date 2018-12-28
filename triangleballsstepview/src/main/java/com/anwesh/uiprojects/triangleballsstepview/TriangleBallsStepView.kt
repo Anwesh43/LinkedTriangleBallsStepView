623585b41df19ead8b3188c47ec2c5a19040504c
Anwesh43 <anweshthecool0@gmail.com>
2018-12-28 09:18:55 +0530

    adding drawTBSNode canvas extension method

--- triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
+++ triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
@@ -14,10 +14,10 @@ import android.app.Activity
 
 val nodes : Int = 5
 val rows : Int = 4
+val movements : Int = 2
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val sizeFactor : Float = 2.6f
-val strokeFactor : Int = 90
 val color : Int = Color.parseColor("#1abc9c")
 
 fun Int.inverse() : Float = 1f / this
@@ -27,4 +27,38 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
-
+fun Canvas.drawTBSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val ballGap : Float = (2 * size) / (rows + 1)
+    val r = ballGap/4
+    paint.color = color
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(90f * sc2)
+    translate(-size, -size)
+    var currY : Float = 0f
+    for (j in 0..(rows - 1)) {
+        val sc : Float = sc1.divideScale(j, rows)
+        if (sc == 0f) {
+            break
+        }
+        val sck1 : Float = sc.divideScale(0, 2)
+        val sck2 : Float = sc.divideScale(1, 2)
+        var currX : Float = 0f
+        for (t in 0..j) {
+            if (t == j) {
+                currX += ballGap * sck2
+            } else {
+                currX += ballGap
+            }
+            drawCircle(currX, currY, r, paint)
+        }
+        currY += ballGap * sck1
+    }
+    restore()
+}
