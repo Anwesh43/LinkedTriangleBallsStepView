568613c937073f7a83ed6cdd4b8a91f0345409fb
Anwesh43 <anweshthecool0@gmail.com>
2018-12-28 08:42:42 +0530

    importing libraries and adding constants

--- triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
+++ triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.triangleballsstepview
 /**
  * Created by anweshmishra on 28/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+
+val nodes : Int = 5
+val rows : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.6f
+val strokeFactor : Int = 90
+val color : Int = Color.parseColor("#1abc9c")
