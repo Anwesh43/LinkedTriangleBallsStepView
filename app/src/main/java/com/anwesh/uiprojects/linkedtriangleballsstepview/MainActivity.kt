c561361eaf4f772eb235f8e743755849849a4d85
Anwesh43 <anweshthecool0@gmail.com>
2018-12-28 09:41:17 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtriangleballsstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtriangleballsstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtriangleballsstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.triangleballsstepview.TriangleBallsStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriangleBallsStepView.create(this)
     }
 }
