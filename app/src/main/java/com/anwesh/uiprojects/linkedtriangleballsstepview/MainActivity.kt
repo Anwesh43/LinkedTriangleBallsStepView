c50d20a360856a1531f107ffee110d1f24d01ab4
Anwesh43 <anweshthecool0@gmail.com>
2018-12-28 09:42:12 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtriangleballsstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtriangleballsstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedtriangleballsstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.triangleballsstepview.TriangleBallsStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TriangleBallsStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
