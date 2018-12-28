2c5cd2d79202a59616b2a1ff8e9626b0c38f83fc
Anwesh43 <anweshthecool0@gmail.com>
2018-12-28 09:20:21 +0530

    adding TriangleBallsStepView class

--- triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
+++ triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
@@ -62,3 +62,21 @@ fun Canvas.drawTBSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TriangleBallsStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
