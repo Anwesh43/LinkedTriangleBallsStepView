7fbc0935f0b7a165280e54d0ffbfe1957eb57ad3
Anwesh43 <anweshthecool0@gmail.com>
2018-12-28 09:33:44 +0530

    using renderer in View

--- triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
+++ triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
@@ -67,14 +67,16 @@ class TriangleBallsStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
