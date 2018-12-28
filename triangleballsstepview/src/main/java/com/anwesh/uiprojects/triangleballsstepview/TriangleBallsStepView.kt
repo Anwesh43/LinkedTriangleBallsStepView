5eef14a30f2d2c07f8729d1a4283a7ebb215e00d
Anwesh43 <anweshthecool0@gmail.com>
2018-12-28 09:30:36 +0530

    adding TriangleBallStep class

--- triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
+++ triangleballsstepview/src/main/java/com/anwesh/uiprojects/triangleballsstepview/TriangleBallsStepView.kt
@@ -169,7 +169,31 @@ class TriangleBallsStepView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class TriangleBallStep(var i : Int) {
+
+        private val root : TBSNode = TBSNode(0)
+        private var curr : TBSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
